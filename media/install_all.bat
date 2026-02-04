fd226d1d3c08808de0d2a50b207061d473ec28a2
Haider Ali <ha5743330@gmail.com>
2026-02-04 06:20:07 +0500

    Added setup files in media folder

---
+++ media/install_all.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Installing all Python libraries...
+pip install -r requirements.txt
+echo.
+echo Installing Node.js modules...
+npm install
+echo.
+echo All Done! Ab aap project run kar sakte hain.
+pause
